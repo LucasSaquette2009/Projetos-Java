950569387fa5fbe4bcd11359dfa473706a0e1859
Lucas <lucassaquette954@gmail.com>
2026-05-29 19:49:40 -0300

    Rename das pastas

--- 05-postagem-composicao/src/application/Main.java
+++ 05-postagem-composicao/src/application/Main.java
@@ -1,4 +1,24 @@
 package application;
 
+import entities.Comment;
+import entities.Post;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Comment comment = new Comment("Massa demais!!");
+        Comment comment2 = new Comment("Parábens pela conquista");
+
+
+        Post post = new Post("Novo projeto!!", "Projeto que desenvolvi no java", 30);
+
+        post.addComment(comment);
+        post.addComment(comment2);
+
+        System.out.println(post);
+
+        post.removeComment(comment);
+
+        System.out.println(post);
+    }
 }
--- 05-postagem-composicao/src/entities/Post.java
+++ 05-postagem-composicao/src/entities/Post.java
@@ -1,4 +1,76 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Post {
+    private String title;
+    private String content;
+    private int likes;
+    private List<Comment> comments = new ArrayList<>();
+
+    public Post(String title, String content, int likes) {
+        this.title = title;
+        this.content = content;
+        this.likes = likes;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public void addComment(Comment comment) {
+        comments.add(comment);
+    }
+
+    public void removeComment(Comment comment) {
+        comments.remove(comment);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("-------------------------\n");
+        sb.append("Title: ").append(title).append("\n");
+        sb.append("Content: ").append(content).append("\n");
+        sb.append("Likes: ").append(likes).append("\n");
+        sb.append("Comments:\n");
+
+        for (Comment c : comments) {
+            sb.append("-").append(c.getText()).append("\n");
+        }
+
+        sb.append("-------------------------");
+
+        return sb.toString();
+    }
 }
